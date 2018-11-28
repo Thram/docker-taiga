9acfe8933a92149c6da3ab9d6af74951d8dac683
Sebastian <sebastian@thankyoupayroll.co.nz>
2018-11-29 02:14:44 +1300

    fix(celery): Fix problem with celeryworker

--- backend/celery_local.py
+++ backend/celery_local.py
@@ -5,6 +5,7 @@ print("Setup init")
 
 env = environ.Env()
 
+# CELERY_IMPORTS = ('taiga.importers.trello.tasks.import_project' )
 RABBITMQ_DEFAULT_USER = env('RABBITMQ_DEFAULT_USER', default='taiga')
 RABBITMQ_DEFAULT_PASS = env('RABBITMQ_DEFAULT_PASS', default='taiga')
 RABBITMQ_DEFAULT_VHOST = env('RABBITMQ_DEFAULT_VHOST', default='taiga')
