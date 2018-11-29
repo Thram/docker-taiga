b269274034769fe4a162aa1d8fadb60fe1d8ae1e
Sebastian <sebastian@thankyoupayroll.co.nz>
2018-11-29 18:04:41 +1300

    fix(frontend): Fix events URL

--- frontend/scripts/entrypoint.sh
+++ frontend/scripts/entrypoint.sh
@@ -5,21 +5,21 @@ set -o pipefail
 
 if [ ! -f /taiga_frontend/conf.json ]; then
     echo "Generating /taiga_frontend/conf.json file..."
-    API_URL=${API_URL:-/api/v1/}
-    DEFAULT_LANGUAGE=${DEFAULT_LANGUAGE:-en}
+    API_URL=${API_URL:-\"/api/v1/\"}
+    DEFAULT_LANGUAGE=${DEFAULT_LANGUAGE:-\"en\"}
     EVENTS_URL=${EVENTS_URL:-null}
     IMPORTERS=${IMPORTERS:-null}
     PUBLIC_REGISTER_ENABLED=${PUBLIC_REGISTER_ENABLED:-true}
     if [[ $IMPORTERS != null ]]; then IMPORTERS="[$IMPORTERS]"; fi
     cat > /taiga_frontend/conf.json <<EOF
 {
-    "api": "$API_URL",
-    "eventsUrl": "$EVENTS_URL",
+    "api": $API_URL,
+    "eventsUrl": $EVENTS_URL,
     "eventsMaxMissedHeartbeats": 5,
     "eventsHeartbeatIntervalTime": 60000,
     "debug": ${DEBUG:-false},
     "debugInfo": ${DEBUG:-false},
-    "defaultLanguage": "$DEFAULT_LANGUAGE",
+    "defaultLanguage": $DEFAULT_LANGUAGE,
     "themes": ["taiga"],
     "defaultTheme": "taiga",
     "importers": $IMPORTERS,
