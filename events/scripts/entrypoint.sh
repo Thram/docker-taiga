326939870226a103351cc553abaa9443dc1be8bf
Sebastian <sebastian@thankyoupayroll.co.nz>
2018-11-29 15:26:00 +1300

    fix(events): Fix events entrypoint script

--- events/scripts/entrypoint.sh
+++ events/scripts/entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 if [ ! -f /taiga_events/config.json ]; then
     echo "Generating /taiga_events/config.json file..."
-    RABBITMQ_USER = ${RABBITMQ_USER:-"taiga"}
-    RABBITMQ_PASSWORD = ${RABBITMQ_PASSWORD:-"taiga"}
-    RABBITMQ_VHOST = ${RABBITMQ_VHOST:-"taiga"}
+    RABBITMQ_USER=${RABBITMQ_USER:-"taiga"}
+    RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:-"taiga"}
+    RABBITMQ_VHOST=${RABBITMQ_VHOST:-"taiga"}
     cat > /taiga_events/config.json <<EOF
 {
     "url": "amqp://$RABBITMQ_USER:$RABBITMQ_PASSWORD@rabbitmq:5672/$RABBITMQ_VHOST",
