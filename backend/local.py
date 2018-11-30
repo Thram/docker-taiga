3472874f73a32d6cd669438b55eb4c9f2ebf1e21
Sebastian <sebastian@thankyoupayroll.co.nz>
2018-11-30 15:23:57 +1300

    fix(django): Fix DJANGO variables

--- backend/local.py
+++ backend/local.py
@@ -11,8 +11,8 @@ PUBLIC_REGISTER_ENABLED = env(
     'PUBLIC_REGISTER_ENABLED', cast=bool, default=True
 )
 
-SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=list, default=['*'])
+DJANGO_SECRET_KEY = env('SECRET_KEY')
+DJANGO_ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=list, default=['*'])
 
 DATABASES = {
     'default': {
